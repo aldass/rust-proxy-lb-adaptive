5c0edeeb02999bdbfb661158ab6bbf3d15f8dfda
AL Dass <AL.Dass@gmail.com>
2024-11-30 10:37:40 -0600

    Add initial implementation of a TCP proxy server with basic client handling

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::net::SocketAddr;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
+
+async fn handle_client(
+    mut inbound: TcpStream,
+    proxy_addr: SocketAddr,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut outbound = TcpStream::connect(proxy_addr).await?;
+
+    let (mut ri, mut wi) = inbound.split();
+    let (mut ro, mut wo) = outbound.split();
+
+    let client_to_server = async {
+        tokio::io::copy(&mut ri, &mut wo).await?;
+        wo.shutdown().await
+    };
+
+    let server_to_client = async {
+        tokio::io::copy(&mut ro, &mut wi).await?;
+        wi.shutdown().await
+    };
+
+    tokio::try_join!(client_to_server, server_to_client)?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listen_addr = "127.0.0.1:8085".parse::<SocketAddr>()?;
+    let proxy_addr = "127.0.0.1:8086".parse::<SocketAddr>()?;
+
+    let listener = TcpListener::bind(listen_addr).await?;
+    println!("Listening on: {}", listen_addr);
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        let proxy_addr = proxy_addr.clone();
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(socket, proxy_addr).await {
+                eprintln!("Error handling client: {}", e);
+            }
+        });
+    }
+}
